e21e345c56c61faf6d930edfdd40cc4fb784df27
tWhite7217 <tommywhite525@gmail.com>
2022-06-15 12:19:41 -0400

    Python validator updated to validate bootstrap-limited model results.
    A commandline option informs the script if the unlimited or limited model was used. I have only confirmed that the unlimited validator is still working. I will need to generate the results csv for bootstrap-limited models before I can test that validator.

---
+++ solution_validator.py
@@ -0,0 +1,198 @@
+import sys
+
+bootstrapping_latency = 12
+
+
+class SolutionValidator:
+    def __init__(
+        self,
+        operation_types,
+        operations,
+        dependencies,
+        bootstrapping_paths,
+        solver_latency,
+        bootstrapped_operations,
+        used_bootstrap_limited_model,
+    ):
+        self.operation_types = operation_types
+        self.dependencies = dependencies
+        self.bootstrapping_paths = bootstrapping_paths
+        self.uncompleted_operations = operations
+        self.bootstrapped_operations = bootstrapped_operations
+        self.solver_latency = solver_latency
+        self.used_bootstrap_limited_model = used_bootstrap_limited_model
+        self.running_operations = {}
+        self.bootstrapping_queue = []
+        self.bootstrapping_operations = {}
+
+    def validate_solution(self):
+        self.bootstrapping_constraints_are_met()
+
+        clock_cycle = -1
+
+        while self.program_is_not_complete():
+            self.bootstrapping_operations = self.decrement_cycles_left_for(
+                self.bootstrapping_operations
+            )
+            self.running_operations = self.decrement_cycles_left_for(
+                self.running_operations
+            )
+
+            self.bootstrapping_operations = self.remove_finished_operations(
+                self.bootstrapping_operations
+            )
+            self.add_necessary_running_operations_to_bootstrapping_queue()
+            self.running_operations = self.remove_finished_operations(
+                self.running_operations
+            )
+            self.start_bootstrapping_ready_operations_from_queue()
+
+            ready_operations = self.get_ready_operations()
+            self.start_running_ready_operations(ready_operations)
+
+            clock_cycle += 1
+
+        if clock_cycle != self.solver_latency:
+            print("Error: The latencies mismatch")
+            print("Solver latency: %d" % self.solver_latency)
+            print("Calculated latency: %d" % clock_cycle)
+        elif self.used_bootstrap_limited_model == "False":
+            print("Unlimited ILP model results are correct")
+            sys.exit(0)
+
+    def bootstrapping_constraints_are_met(self):
+        for (operation, path_list) in self.bootstrapping_paths.items():
+            for path in path_list:
+                path_satisfied = False
+                for bootstrapped_operation in self.bootstrapped_operations:
+                    if bootstrapped_operation in path:
+                        path_satisfied = True
+                        break
+                if not path_satisfied:
+                    print("Error: The following path was not satsified")
+                    print(path)
+                    sys.exit(0)
+
+    def program_is_not_complete(self):
+        return (
+            len(self.uncompleted_operations) > 0
+            or len(self.running_operations) > 0
+            or len(self.bootstrapping_operations) > 0
+        )
+
+    def decrement_cycles_left_for(self, operation_dict):
+        for (operation, cycles_left) in operation_dict.items():
+            operation_dict[operation] = cycles_left - 1
+        return operation_dict
+
+    def remove_finished_operations(self, operation_dict):
+        return {
+            operation: cycles_left
+            for (operation, cycles_left) in operation_dict.items()
+            if cycles_left > 0
+        }
+
+    def finish_operation_or_begin_bootstrapping(self, operation):
+        if operation not in self.bootstrapped_operations:
+            del self.running_operations[operation]
+        else:
+
+            del self.running_operations[operation]
+
+    def add_necessary_running_operations_to_bootstrapping_queue(self):
+        self.bootstrapping_queue += [
+            operation
+            for (operation, cycles_left) in self.running_operations.items()
+            if operation in self.bootstrapped_operations and cycles_left == 0
+        ]
+
+    def start_bootstrapping_ready_operations_from_queue(self):
+        if self.used_bootstrap_limited_model == "True":
+            operations_to_remove = []
+            for operation in self.bootstrapping_queue:
+                if self.clock_cycle_matches_operation_bootstrapping_start_time(
+                    operation
+                ):
+                    [
+                        core_is_available,
+                        bootstrapping_operation,
+                    ] = self.check_if_bootstrapping_core_is_available(operation)
+                    if core_is_available:
+                        self.bootstrapping_operations[operation] = bootstrapping_latency
+                        operations_to_remove.append(operation)
+                    else:
+                        self.print_bootstrapping_core_is_not_available_error(
+                            operation, bootstrapping_operation
+                        )
+                        sys.exit(0)
+                elif clock_cycle_is_later_than_operation_bootstrapping_start_time(
+                    operation
+                ):
+                    print_missed_bootstrapping_deadline_error(operation)
+                    sys.exit(0)
+            for operation in operations_to_remove:
+                self.bootstrapping_queue.remove(operation)
+        else:
+            for operation in self.bootstrapping_queue:
+                self.bootstrapping_operations[operation] = bootstrapping_latency
+            self.bootstrapping_queue = []
+
+    def clock_cycle_matches_operation_bootstrapping_start_time(self, operation):
+        return (
+            clock_cycle
+            == self.bootstrapping_start_times[bootstrapping_operations.index(operation)]
+        )
+
+    def check_if_operation_bootstrapping_core_is_available(self, operation):
+        for bootstrapping_operation in self.bootstrapping_operations:
+            if self.operations_bootstrap_on_same_core(
+                operation, bootstrapping_operation
+            ):
+                return [False, bootstrapping_operation]
+        return [True, None]
+
+    def operations_bootstrap_on_same_core(self, op1, op2):
+        return (
+            self.cores_used_to_bootstrap[bootstrapping_operations.index(op1)]
+            == self.cores_used_to_bootstrap[bootstrapping_operations.index(op2)]
+        )
+
+    def print_bootstrapping_core_is_not_available_error(
+        self, operation, bootrsapping_operation
+    ):
+        print(
+            "Error: Bootstrapping operation %s was not completed before operation %s started bootstrapping on core %d"
+            % (
+                bootrsapping_operation,
+                operation,
+                self.cores_used_to_bootstrap[
+                    self.bootstrapping_operations.index(operation)
+                ],
+            )
+        )
+
+    def get_ready_operations(self):
+        ready_operations = {
+            operation: operation_type
+            for (operation, operation_type) in self.uncompleted_operations.items()
+            if self.operation_is_ready(operation)
+        }
+        return ready_operations
+
+    def operation_is_ready(self, operation):
+        if operation in self.dependencies:
+            for dependency in self.dependencies[operation]:
+                if (
+                    dependency in self.uncompleted_operations
+                    or dependency in self.running_operations
+                    or dependency in self.bootstrapping_operations
+                ):
+                    return False
+        return True
+
+    def start_running_ready_operations(self, ready_operations):
+        for (operation, operation_type) in ready_operations.items():
+            self.running_operations[operation] = int(
+                self.operation_types[operation_type][1]
+            )
+            del self.uncompleted_operations[operation]
---
+++ validate_ilp_solution.py
@@ -0,0 +1,40 @@
+import sys
+from DDGs.custom_ddg_format_parser import InputParser
+from solution_validator import SolutionValidator
+
+bootstrapping_path_length = 3
+
+input_filename = sys.argv[1]
+results_filename = sys.argv[2]
+used_bootstrap_limited_model = sys.argv[3]
+
+input_parser = InputParser(bootstrapping_path_length, True)
+input_parser.parse_input(input_filename)
+
+results_file = open(results_filename, "r")
+results_lines = results_file.readlines()
+results_file.close()
+
+bootstrapped_operations = results_lines[0].split(",")
+solver_latency = int(results_lines[1])
+cores_used_to_bootstrap = []
+bootstrapping_start_times = []
+finish_times = []
+if used_bootstrap_limited_model == "True":
+    cores_used_to_bootstrap = [int(core) for core in results_lines[2].split(",")]
+    bootstrapping_start_times = [
+        int(start_time) for start_time in results_lines[3].split(",")
+    ]
+    finish_times = [int(finish_time) for finish_time in results_lines[4].split(",")]
+
+solution_validator = SolutionValidator(
+    input_parser.operation_types,
+    input_parser.operations,
+    input_parser.dependencies,
+    input_parser.bootstrapping_paths,
+    solver_latency,
+    bootstrapped_operations,
+    used_bootstrap_limited_model
+)
+
+solution_validator.validate_solution()
